187de8c7c49116e8f49db5f0e5e3e61745e31a3d
janbaangkn <jan.baan@fokker.com>
2024-01-09 11:54:31 +0100

    some more renaming

--- penelope_aerospace_pl_interfaces_examples/penelope_aerospace_pl_interfaces_examples/fokker_action_server.py
+++ penelope_aerospace_pl_interfaces_examples/penelope_aerospace_pl_interfaces_examples/fokker_action_server.py
@@ -1,8 +1,11 @@
 import time
 
 import rclpy
-from penelope_aerospace_pl_msgs.action import InfraredThermographyInspect
-from penelope_aerospace_pl_msgs.msg import InfraredThermographyState, ResultCodes
+from penelope_aerospace_pl_msgs.action import CobotOp
+from penelope_aerospace_pl_msgs.msg import CobotDrillingHole
+from penelope_aerospace_pl_msgs.msg import CobotDrillingHoleArray
+from penelope_aerospace_pl_msgs.msg import CobotState
+from penelope_aerospace_pl_msgs.msg import ResultCodes
 from rclpy.action import ActionServer
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -11,7 +14,7 @@ from sensor_msgs.msg import Image
 class FokkerActionServer(Node):
     def __init__(self):
         super().__init__("fokker_action_server")
-        self._action_server = ActionServer(self, InfraredThermographyInspect, "example_action", self.execute_callback)
+        self._action_server = ActionServer(self, CobotOp, "Cobot Operation", self.execute_callback)
 
     def execute_callback(self, goal_handle):
         self.get_logger().info("Executing example goal...")
@@ -26,17 +29,18 @@ class FokkerActionServer(Node):
         for i in range(0, 100):
             self.get_logger().info(f"Feedback itteration: {i}")
 
+            cobot_state = CobotState()
             # ...
 
             # Create a feedback message
-            feedback_msg = InfraredThermographyInspect.Feedback()
+            feedback_msg = CobotOp.Feedback()
 
             # Fill in the feedback message
             feedback_msg.measurement_status = list(range(0 + i, 5 + i))
 
             # Set process state and module state
             #   note: 2 ways to set the "child" message are shown, both can be used
-            process_state = InfraredThermographyState()
+            process_state = CobotDrillingHole()
             process_state.data = 1
             feedback_msg.process_state = process_state
 
@@ -50,7 +54,7 @@ class FokkerActionServer(Node):
         goal_handle.succeed()
 
         ## Provide result ##
-        result = InfraredThermographyInspect.Result()
+        result = CobotOp.Result()
         images = [Image() for _ in range(0, 5)]  # Images are empty for the example
 
         result.images = images
