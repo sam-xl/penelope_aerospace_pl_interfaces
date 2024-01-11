d7caf96291744e90b1fdf9bad4ab1b00631af1be
janbaangkn <jan.baan@fokker.com>
2024-01-11 17:19:55 +0100

    started writing the send_message_to_cobot functions

--- penelope_aerospace_pl_interfaces_examples/penelope_aerospace_pl_interfaces_examples/fokker_action_server.py
+++ penelope_aerospace_pl_interfaces_examples/penelope_aerospace_pl_interfaces_examples/fokker_action_server.py
@@ -21,6 +21,37 @@ from rclpy.action import ActionServer
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 
+CLOSE_TAG = ")"
+UID_TAG = "uid("
+STORAGE_TAG = "storage("
+PRODUCT_TAG = "product("
+LOCATIONS_TAG = "locations("
+MAX_OBST_HEIGHT_TAG = "max_obstacle_height("
+APPR_POS_UID_TAG = "approach_pos_uid("
+HOLE_LOCATION_CONT_TAG = "hole_location_container("
+HOLE_LOCATION_TAG = "hole_location("
+POSE_TAG = "pose("
+POSE_PX_TAG = "pose_p_x("
+POSE_PY_TAG = "pose_p_y("
+POSE_PZ_TAG = "pose_p_z("
+POSE_OX_TAG = "pose_o_x("
+POSE_OY_TAG = "pose_o_y("
+POSE_OZ_TAG = "pose_o_z("
+POSE_OW_TAG = "pose_o_w("
+DIAM_TAG = "diam("
+STACK_T_TAG = "stack_thickness_tag("
+DRILL_JIG_DIST_TAG = "drill_jig_dist("
+DRILLED_TAG = "drilled("
+LAYERS_TAG = "layers("
+MAT_LAYER_TAG = "material_layer("
+LAYER_THICKNESS_TAG = "layer_thickness("                               
+DRILL_SPEED_TAG = "drill_speed("                           
+DRILL_FEED_TAG = "drill_feed("                            
+LOWER_TORQUE_LIMIT_TAG = "lower_torque_limit("             
+UPPER_TORQUE_LIMIT_TAG = "upper_torque_limit("            
+TORQUE_THRESHOLD_TAG = "torque_threshold("                      
+MAX_TEMPF_CLAMP_FORCE_TAG = "max_tempf_clamp_force("
+
 
 class FokkerActionServer(Node):
     def __init__(self):
@@ -32,17 +63,7 @@ class FokkerActionServer(Node):
 
         # Accessing the request data and send to cobot
         # This instantiates and populates the classes in the Cobot controller
-        self._send_storage_to_cobot(goal_handle.request.storage)           # list of storage location containers
-        self._send_products_to_cobot(goal_handle.request.products)         # list of product containers with holes
-        self._send_waypoints_to_cobot(goal_handle.request.waypoints)       # list of defined waypoints
-        self._send_actions_to_cobot(goal_handle.request.actions)           # list of defined actions
-        self._send_drill_tasks_to_cobot(goal_handle.request.drill_tasks)   # list of holes to be drilled
-        self._send_fasteners_to_cobot(goal_handle.request.fasteners)       # list of available fasteners
-        self._send_tempfs_to_cobot(goal_handle.request.tempfs)             # list of available temporary fasteners
-        self._send_docking_pos_to_cobot(goal_handle.request.docking_pos)   # list of available docking positions for End Effectors
-        self._send_ee_to_cobot(goal_handle.request.ee)                     # list of available End Effectors
-
-        # TODO check if everything was received well
+        self._send_goal_handle_to_cobot(goal_handle)
 
         # start sending the uid of the actions to the cobot 
         # to execute these actions
@@ -65,13 +86,7 @@ class FokkerActionServer(Node):
                 feedback_msg = CobotOp.Feedback()
 
                 # Fill in the feedback message
-                feedback_msg.actions_out = self._create_actions_from_cobot_output()  
-                feedback_msg.drill_tasks_out = self._create_drill_tasks_from_cobot_output()
-                feedback_msg.tempfs_out = self._create_tempfs_from_cobot_output()
-                feedback_msg.drill_tasks_out = self._create_drill_tasks_from_cobot_output()
-                feedback_msg.ee_out = self._create_ee_from_cobot_output()
-                feedback_msg.result_code = 1 # TODO determie sensible result_code 
-                feedback_msg.message = ""
+                self._populate_feedback_message_from_cobot_output(feedback_msg)
 
                 # Send feedback
                 goal_handle.publish_feedback(feedback_msg)
@@ -94,67 +109,251 @@ class FokkerActionServer(Node):
 
         return result
     
+    # Function to send goal_handle contents to the cobot controller
+    def _send_goal_handle_to_cobot(self, goal_handle_in):
+        func_output = [0] * 8
+
+        # list of storage location containers
+        func_output[0] = self._send_storage_to_cobot(goal_handle_in.request.storage)           
+        if func_output[0] < 0:
+            self.get_logger().info("Failed to send storage to cobot.")
+
+        # list of product containers with holes
+        func_output[1] = self._send_products_to_cobot(goal_handle_in.request.products)         
+        if func_output[1] < 0:
+            self.get_logger().info("Failed to send products to cobot.")
+
+        # list of defined waypoints
+        func_output[2] = self._send_waypoints_to_cobot(goal_handle_in.request.waypoints)       
+        if func_output[2] < 0:
+            self.get_logger().info("Failed to send waypoints to cobot.")
+
+        # list of defined actions
+        func_output[3] = self._send_actions_to_cobot(goal_handle_in.request.actions)           
+        if func_output[3] < 0:
+            self.get_logger().info("Failed to send actions to cobot.")
+
+        # list of holes to be drilled
+        func_output[4] = self._send_drill_tasks_to_cobot(goal_handle_in.request.drill_tasks)   
+        if func_output[4] < 0:
+            self.get_logger().info("Failed to send drill_tasks to cobot.")
+
+        # list of available fasteners
+        func_output[5] = self._send_fasteners_to_cobot(goal_handle_in.request.fasteners)       
+        if func_output[5] < 0:
+            self.get_logger().info("Failed to send fasteners to cobot.")
+
+        # list of available temporary fasteners
+        func_output[6] = self._send_tempfs_to_cobot(goal_handle_in.request.tempfs)             
+        if func_output[6] < 0:
+            self.get_logger().info("Failed to send tempfs to cobot.")
+
+        # list of available docking positions for End Effectors
+        func_output[7] = self._send_docking_pos_to_cobot(goal_handle_in.request.docking_pos)   
+        if func_output[7] < 0:
+            self.get_logger().info("Failed to send docking_pos to cobot.")
+
+        # list of available End Effectors
+        func_output[8] = self._send_ee_to_cobot(goal_handle_in.request.ee)                     
+        if func_output[8] < 0:
+            self.get_logger().info("Failed to send ee to cobot.")
+
+        return min(func_output)
+        
     # Function to send list of storage location containers to the cobot controller
-    def _send_storage_to_cobot(storage_in):
+    # AssemblyHoleLocationContainer
+    def _send_storage_to_cobot(self, storage_in):
+        str = STORAGE_TAG + self._get_hole_location_container_to_cobot_str(storage_in) + CLOSE_TAG
+        self._send_msg_to_cobot(str)
         return 1
+
+    # get message string for AssemblyHoleLocationContainer
+    def _get_hole_location_container_to_cobot_str(self, cont_in):
+        str = HOLE_LOCATION_CONT_TAG 
+        
+        # uid of the container
+        str = str + UID_TAG + cont_in.uid + CLOSE_TAG
+
+        # list of hole locations
+        str = str + LOCATIONS_TAG
+        for ahl in cont_in.locations:
+            str = str + self._get_hole_location_to_cobot_str(ahl)
+        str = str + CLOSE_TAG
+
+        # max_obstacle_height
+        str = str + MAX_OBST_HEIGHT_TAG + cont_in.max_obstacle_height + CLOSE_TAG
+
+        # approach_pos_uid
+        str = str + APPR_POS_UID_TAG + cont_in.approach_pos_uid + CLOSE_TAG 
+
+        return str + CLOSE_TAG
+    
+    # get message string for AssemblyHoleLocation
+    def _get_hole_location_to_cobot_str(self, loc_in):
+        str = HOLE_LOCATION_TAG 
+        # uid of the hole position: string uid
+        str = str + UID_TAG + loc_in.uid + CLOSE_TAG                         
+
+        # Nominal location of the hole: geometry_msgs/Pose nom_pos
+        str = str + self._get_pose_str(loc_in.nom_pos)             
+
+        # Diameter of the hole: float32 diam
+        str = str + DIAM_TAG + loc_in.diam + CLOSE_TAG                
+
+        # Total thickness at the hole
+        # Must be equal to the sum of layer thicknesses: float32 stack_t
+        str = str + STACK_T_TAG + loc_in.stack_t + CLOSE_TAG        
+
+        # distance of the drill jig hole entry above the surface: float32 drill_jig_dist                  
+        str = str + DRILL_JIG_DIST_TAG + loc_in.drill_jig_dist + CLOSE_TAG
+
+        # Whether or not the hole has been drilled: bool drilled
+        str = str + DRILLED_TAG + loc_in.drilled + CLOSE_TAG             
+
+        # Material definition for each layer: AssemblyMaterialLayer[] layers
+        str = str + LAYERS_TAG
+        for layer in loc_in.layers:
+            str = str + self._get_material_layer_to_cobot_str(layer)
+        str = str + CLOSE_TAG
+
+        return str + CLOSE_TAG
+    
+    # get message string for geometry_msgs/Pose
+    def _get_pose_str(self, pose_in):
+        str = POSE_TAG 
+
+        # Get position
+        str = str + POSE_PX_TAG + pose_in.position.x + CLOSE_TAG
+        str = str + POSE_PY_TAG + pose_in.position.y + CLOSE_TAG
+        str = str + POSE_PZ_TAG + pose_in.position.z + CLOSE_TAG
+
+        # Get orientation
+        str = str + POSE_OX_TAG + pose_in.orientation.x + CLOSE_TAG
+        str = str + POSE_OY_TAG + pose_in.orientation.y + CLOSE_TAG
+        str = str + POSE_OZ_TAG + pose_in.orientation.z + CLOSE_TAG
+        str = str + POSE_OW_TAG + pose_in.orientation.w + CLOSE_TAG
+
+        return str + CLOSE_TAG
+    
+    # get message string for AssemblyMaterialLayer
+    def _get_material_layer_to_cobot_str(self, layer_in):
+        str = MAT_LAYER_TAG 
+
+        # uid of the layer: string uid   
+        str = str + UID_TAG + layer_in.uid + CLOSE_TAG
+
+        # Thickness of the layer:float32 thickness 
+        str = str + LAYER_THICKNESS_TAG + layer_in.thickness + CLOSE_TAG
+
+        # Drill specifications during the drilling in this layer
+        # Speed of the drill:float32 speed     
+        str = str + DRILL_SPEED_TAG+ layer_in.speed + CLOSE_TAG  
+
+        # Feed rate of the drill:float32 feed  
+        str = str + DRILL_FEED_TAG + layer_in.feed + CLOSE_TAG 
+
+        # Lower limit(s) of the torque bandwidth(s):float32 lower_torque_limits  
+        str = str + LOWER_TORQUE_LIMIT_TAG + layer_in.lower_torque_limits + CLOSE_TAG
+
+        # Upper limit(s) of the torque bandwidth(s):float32 upper_torque_limits  
+        str = str + UPPER_TORQUE_LIMIT_TAG + layer_in.upper_torque_limits + CLOSE_TAG  
+
+        # Torque level/threshold to switch to the next layer in the stack: float32 threshold 
+        str = str + TORQUE_THRESHOLD_TAG + layer_in.threshold + CLOSE_TAG 
+
+        # Temporary fastening specifications for this layer
+        # Maximum temporary fastener clamp force:float32 max_clamp_force                 
+        str = str + MAX_TEMPF_CLAMP_FORCE_TAG + layer_in.max_clamp_force + CLOSE_TAG
+
+        return str + CLOSE_TAG
                    
     # Function to send list of product containers with holes to the cobot controller
-    def _send_products_to_cobot(products_in):
+    def _send_products_to_cobot(self, products_in):
+        str = PRODUCT_TAG + self._get_hole_location_container_to_cobot_str(products_in) + CLOSE_TAG
+        self._send_msg_to_cobot(str)
         return 1
 
     # Function to send list of defined waypoints to the cobot controller
-    def _send_waypoints_to_cobot(waypoints_in):
+    def _send_waypoints_to_cobot(self, waypoints_in):
         return 1 
 
     # Function to send list of defined actions to the cobot controller
-    def _send_actions_to_cobot(actions_in): 
+    def _send_actions_to_cobot(self, actions_in): 
         return 1 
 
     # Function to send list of holes to be drilled to the cobot controller
-    def _send_drill_tasks_to_cobot(drill_tasks_in): 
+    def _send_drill_tasks_to_cobot(self, drill_tasks_in): 
         return 1
 
     # Function to send list of available fasteners to the cobot controller
-    def _send_fasteners_to_cobot(fasteners_in):  
+    def _send_fasteners_to_cobot(self, fasteners_in):  
         return 1 
 
     # Function to send list of available temporary fasteners to the cobot controller
-    def _send_tempfs_to_cobot(tempfs_in): 
+    def _send_tempfs_to_cobot(self, tempfs_in): 
         return 1
 
     # Function to send list of available docking positions for End Effectors to the cobot controller
-    def _send_docking_pos_to_cobot(docking_pos_in): 
+    def _send_docking_pos_to_cobot(self, docking_pos_in): 
         return 1
       
     # Function to send list of available End Effectors to the cobot controller
-    def _send_ee_to_cobot(ee_in):  
+    def _send_ee_to_cobot(self, ee_in):  
         return 1   
 
     # Function to send execution commands to the cobot controller
     # Excution commands are given by sending the uid of the action
     # to be executed.
-    def _send_execution_action_uid_to_cobot(uid_in):  
-        return 1    
-
-    # Function to create list of defined actions to the cobot controller
-    def _create_actions_from_cobot_output(): 
+    def _send_execution_action_uid_to_cobot(self, uid_in):  
         return 1 
 
-    # Function to create list of holes to be drilled to the cobot controller
-    def _create_drill_tasks_from_cobot_output(): 
-        return 1
-
-    # Function to create list of available fasteners to the cobot controller
-    def _create_fasteners_from_cobot_output():  
+    # function to send a message to the cobot
+    # the message will be received by the cobot controller
+    # the message will be used to instantiate/popuate classes
+    # in the cobot conroller
+    def _send_msg_to_cobot(str_in):
+        #TODO send to cobot
         return 1 
 
-    # Function to create list of available temporary fasteners to the cobot controller
-    def _create_tempfs_from_cobot_output(): 
+    # Function to populate feedback message based on information from the cobot controller
+    def _populate_feedback_message_from_cobot_output(self, feedback_msg):
+        
+        feedback_msg.actions_out = self._create_actions_from_cobot_output()  
+        feedback_msg.drill_tasks_out = self._create_drill_tasks_from_cobot_output()
+        feedback_msg.tempfs_out = self._create_tempfs_from_cobot_output()
+        feedback_msg.drill_tasks_out = self._create_fasteners_from_cobot_output()
+        feedback_msg.ee_out = self._create_ee_from_cobot_output()
+        
+        feedback_msg.result_code = 1 # TODO determie sensible result_code 
+        
+        feedback_msg.message = ""  # TODO make message 
+
         return 1
+
+    # Function to create list of defined actions based on information from the cobot controller
+    def _create_actions_from_cobot_output(self): 
+        lst = []  # [AssemblyAction]
+        return lst
+
+    # Function to create list of holes to be drilled based on information from the cobot controller
+    def _create_drill_tasks_from_cobot_output(self): 
+        lst = []  # [AssemblyDrill]
+        return lst
+
+    # Function to create list of available fasteners based on information from the cobot controller
+    def _create_fasteners_from_cobot_output(self):  
+        lst = []  # [AssemblyFast]
+        return lst
+
+    # Function to create list of available temporary fasteners based on information from the cobot controller
+    def _create_tempfs_from_cobot_output(self): 
+        lst = []  # [AssemblyTempFast]
+        return lst
       
-    # Function to create list of available End Effectors to the cobot controller
-    def _create_ee_from_cobot_output():  
-        return 1            
+    # Function to create list of available End Effectors based on information from the cobot controller
+    def _create_ee_from_cobot_output(self):  
+        lst = []  # [AssemblyEe]
+        return lst            
 
 
 def main(args=None):
