67995448d1f993fdaf141b16632f89f8fe91798a
janbaangkn <jan.baan@fokker.com>
2024-01-11 13:59:03 +0100

    started defining the server

--- penelope_aerospace_pl_interfaces_examples/penelope_aerospace_pl_interfaces_examples/fokker_action_server.py
+++ penelope_aerospace_pl_interfaces_examples/penelope_aerospace_pl_interfaces_examples/fokker_action_server.py
@@ -30,29 +30,57 @@ class FokkerActionServer(Node):
     def execute_callback(self, goal_handle):
         self.get_logger().info("Executing FokkerActionServer...")
 
-        # Accessing the request data
-        holes_specification = goal_handle.request.holes_specification
-        filename = goal_handle.request.config_file
+        # Accessing the request data and send to cobot
+        # This instantiates and populates the classes in the Cobot controller
+        self._send_storage_to_cobot(goal_handle.request.storage)           # list of storage location containers
+        self._send_products_to_cobot(goal_handle.request.products)         # list of product containers with holes
+        self._send_waypoints_to_cobot(goal_handle.request.waypoints)       # list of defined waypoints
+        self._send_actions_to_cobot(goal_handle.request.actions)           # list of defined actions
+        self._send_drill_tasks_to_cobot(goal_handle.request.drill_tasks)   # list of holes to be drilled
+        self._send_fasteners_to_cobot(goal_handle.request.fasteners)       # list of available fasteners
+        self._send_tempfs_to_cobot(goal_handle.request.tempfs)             # list of available temporary fasteners
+        self._send_docking_pos_to_cobot(goal_handle.request.docking_pos)   # list of available docking positions for End Effectors
+        self._send_ee_to_cobot(goal_handle.request.ee)                     # list of available End Effectors
 
-        # ...
+        # TODO check if everything was received well
 
-        ## Provide regular feedback ##
-        for i in range(0, 100):
-            self.get_logger().info(f"Feedback itteration: {i}")
+        # start sending the uid of the actions to the cobot 
+        # to execute these actions
+        action_number = 0
+        for action in goal_handle.request.actions:
+            action_number = action_number + 1
+            self.get_logger().info(f"Start action: {action_number} " + action.uid)
+            self._send_execution_action_uid_to_cobot(action.uid)
 
-            action_file = CobotOp()
-            # ...
+            # Provide regular feedback during each execution
+            iteration_number = 0
+            while iteration_number >= 0:
+                iteration_number = iteration_number + 1
+                self.get_logger().info(f"Feedback iteration: {action_number}.{iteration_number}")
 
-            # Create a feedback message
-            feedback_msg = CobotOp.Feedback()
+                action_file = CobotOp()
+                # ...
 
-            # Fill in the feedback message
+                # Create a feedback message
+                feedback_msg = CobotOp.Feedback()
 
+                # Fill in the feedback message
+                feedback_msg.actions_out = self._create_actions_from_cobot_output()  
+                feedback_msg.drill_tasks_out = self._create_drill_tasks_from_cobot_output()
+                feedback_msg.tempfs_out = self._create_tempfs_from_cobot_output()
+                feedback_msg.drill_tasks_out = self._create_drill_tasks_from_cobot_output()
+                feedback_msg.ee_out = self._create_ee_from_cobot_output()
+                feedback_msg.result_code = 1 # TODO determie sensible result_code 
+                feedback_msg.message = ""
 
-            # Send feedback
-            goal_handle.publish_feedback(feedback_msg)
+                # Send feedback
+                goal_handle.publish_feedback(feedback_msg)
 
-            time.sleep(0.1)  # Just to slow the feedback down
+                time.sleep(0.1)  # Just to slow the feedback down
+
+                if feedback_msg.actions_out(action_number-1).state == AssemblyActionState.SUCCESS:
+                    # Will stop the while loop and continue to the next action
+                    iteration_number = -10
 
         # Indicate the action succeeded (this does not indicate succes!)
         goal_handle.succeed()
@@ -65,6 +93,68 @@ class FokkerActionServer(Node):
         result.message = ResultCodes.SUCCES
 
         return result
+    
+    # Function to send list of storage location containers to the cobot controller
+    def _send_storage_to_cobot(storage_in):
+        return 1
+                   
+    # Function to send list of product containers with holes to the cobot controller
+    def _send_products_to_cobot(products_in):
+        return 1
+
+    # Function to send list of defined waypoints to the cobot controller
+    def _send_waypoints_to_cobot(waypoints_in):
+        return 1 
+
+    # Function to send list of defined actions to the cobot controller
+    def _send_actions_to_cobot(actions_in): 
+        return 1 
+
+    # Function to send list of holes to be drilled to the cobot controller
+    def _send_drill_tasks_to_cobot(drill_tasks_in): 
+        return 1
+
+    # Function to send list of available fasteners to the cobot controller
+    def _send_fasteners_to_cobot(fasteners_in):  
+        return 1 
+
+    # Function to send list of available temporary fasteners to the cobot controller
+    def _send_tempfs_to_cobot(tempfs_in): 
+        return 1
+
+    # Function to send list of available docking positions for End Effectors to the cobot controller
+    def _send_docking_pos_to_cobot(docking_pos_in): 
+        return 1
+      
+    # Function to send list of available End Effectors to the cobot controller
+    def _send_ee_to_cobot(ee_in):  
+        return 1   
+
+    # Function to send execution commands to the cobot controller
+    # Excution commands are given by sending the uid of the action
+    # to be executed.
+    def _send_execution_action_uid_to_cobot(uid_in):  
+        return 1    
+
+    # Function to create list of defined actions to the cobot controller
+    def _create_actions_from_cobot_output(): 
+        return 1 
+
+    # Function to create list of holes to be drilled to the cobot controller
+    def _create_drill_tasks_from_cobot_output(): 
+        return 1
+
+    # Function to create list of available fasteners to the cobot controller
+    def _create_fasteners_from_cobot_output():  
+        return 1 
+
+    # Function to create list of available temporary fasteners to the cobot controller
+    def _create_tempfs_from_cobot_output(): 
+        return 1
+      
+    # Function to create list of available End Effectors to the cobot controller
+    def _create_ee_from_cobot_output():  
+        return 1            
 
 
 def main(args=None):
